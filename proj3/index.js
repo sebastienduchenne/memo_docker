4e8f0d0d1745c2c583e1b2aebc1aabed7b54113d
sebastienduchenne <duchenne.sebastien@gmail.com>
2020-08-19 22:36:47 +0200

    temp

--- proj3/index.js
+++ proj3/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
 
-var urlmongo = "mongodb://localhost/db"; 
 
-mongoose.connect(urlmongo);
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 var db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'Erreur lors de la connexion')); 
@@ -20,4 +24,11 @@ var Articles = mongoose.model('articles', articlesSchema);
 
 Articles.find(function(err, res){
   res.forEach(element => console.log(element))
-}); 
+});
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
